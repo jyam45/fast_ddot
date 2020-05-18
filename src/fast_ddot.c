b1dd3cc90443f29fa0237ccf08ff0a3b40db25ed
山本純一 <yamamoto_juni-chi@yamamotojunichinoMacBook-Pro.local>
2020-05-19 08:21:05 +0900

    Change temporal value's data type : double -> long double

--- src/fast_ddot.c
+++ src/fast_ddot.c
@@ -23,17 +23,17 @@ double fast_ddot(size_t n, const double* x, const double* y)
 	/*
 	    YMM regiser variables
 	*/
-	double ymm0,ymm1,ymm2,ymm3;
-	double ymm4,ymm5,ymm6,ymm7;
-	double ymm8,ymm9,ymm10,ymm11;
-	double ymm12,ymm13,ymm14,ymm15;
+	long double ymm0,ymm1,ymm2,ymm3;
+	long double ymm4,ymm5,ymm6,ymm7;
+	long double ymm8,ymm9,ymm10,ymm11;
+	long double ymm12,ymm13,ymm14,ymm15;
 
 	/*
 	    Unrolled Indeces
 	*/
 	size_t n_unroll;
 
-	double value = 0e0;
+	long double value = 0e0;
 	if( !align ){
 		/* aligiment case 0 */
 		ymm12 = 0e0;
@@ -61,9 +61,6 @@ double fast_ddot(size_t n, const double* x, const double* y)
 			x+=4;
 			y+=4;
 		}
-		value = ymm12 + ymm13 + ymm14 + ymm15;
-		ymm12 = 0e0;
-		ymm13 = 0e0;
 		if( n & 2 ){
 			ymm0  = *(x  ); // Load
 			ymm1  = *(x+1); // Load
@@ -76,8 +73,6 @@ double fast_ddot(size_t n, const double* x, const double* y)
 			x+=2;
 			y+=2;
 		}
-		value = value + ymm12 + ymm13;
-		ymm12 = 0e0;
 		if( n & 1 ){
 			ymm0  = *x; // Load
 			ymm4  = *y; // Load
@@ -86,9 +81,7 @@ double fast_ddot(size_t n, const double* x, const double* y)
 			x++;
 			y++;
 		}
-		value = value + ymm12;
-		//value = ymm12 + ymm13 + ymm14 + ymm15;
-		//value = ymm12;
+		value = ymm12 + ymm13 + ymm14 + ymm15;
 	}else{
 		/* aligiment case 1,2 or 3 */
 		ymm12 = 0e0;
@@ -116,9 +109,6 @@ double fast_ddot(size_t n, const double* x, const double* y)
 			x+=4;
 			y+=4;
 		}
-		value = ymm12 + ymm13 + ymm14 + ymm15;
-		ymm12 = 0e0;
-		ymm13 = 0e0;
 		if( n & 2 ){
 			ymm0  = *(x  ); // Load
 			ymm1  = *(x+1); // Load
@@ -131,8 +121,6 @@ double fast_ddot(size_t n, const double* x, const double* y)
 			x+=2;
 			y+=2;
 		}
-		value = value + ymm12 + ymm13;
-		ymm12 = 0e0;
 		if( n & 1 ){
 			ymm0  = *x; // Load
 			ymm4  = *y; // Load
@@ -141,9 +129,7 @@ double fast_ddot(size_t n, const double* x, const double* y)
 			x++;
 			y++;
 		}
-		value = value + ymm12;
-		//value = ymm12 + ymm13 + ymm14 + ymm15;
-		//value = ymm12;
+		value = ymm12 + ymm13 + ymm14 + ymm15;
 	}
 	return value;
 }
